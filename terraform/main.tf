8d36de4847aee0a306cc99e0007e36eeadc7ab87
akash89loadrite <akash.chandra@goloadrite.com>
2022-12-15 14:13:55 +1300

    Create sanbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "aws.root+sandbox@goloadrite.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aws.root+sandbox@goloadrite.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
